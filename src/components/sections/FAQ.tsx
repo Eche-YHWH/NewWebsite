4de14d11a99059d5e14a99becd476ca14c8ed13a
Eche-YHWH <franciseche9@gmail.com>
2026-02-04 21:23:28 +0100

    Update new

--- src/components/sections/FAQ.tsx
+++ src/components/sections/FAQ.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import Container from "../ui/Container";
 import Reveal from "../ui/Reveal";
 import { Plus, Minus, HelpCircle } from "lucide-react";
